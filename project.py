141454ff3dc08d3d35013f517686f6b47bc5f25b
utkarsh24092409 <utkarsh.bhalwankar@gmail.com>
2025-05-08 12:29:17 +0530

    login and signup setup done

--- project.py
+++ project.py
@@ -17,10 +17,10 @@ class Chatbot:
                        4. press 4 to message a friend 
                     5. press 5 to exit ''')
         
-        if user_input == '1':
-            pass 
+        if user_input == "1":
+            self.signup()
         elif user_input == '2':
-            pass
+            self.login()
         elif user_input == '3':
             pass
         elif user_input == '4':
@@ -28,6 +28,28 @@ class Chatbot:
         else :
             exit() 
 
+    def signup(self):
+        user_email = input("enter your username here--->   ")
+        pass_word  = input("enter your password here--->   ")
+        self.username = user_email
+        self.password = pass_word
+        print("you have successfully signed up")
+        print("\n")
+        self.main()
+    
+    def login(self):
+        if self.username == "" or self.password == "":
+            print("please signup by pressing 1 first")
+        else:
+            uname= input("enter your username here--->")
+            pwd= input("enter your password here--->")
+            if self.username == uname and self.password == pwd:
+                print("you have successfully logged in")
+                self.loggeing = True
+            else:
+                print("please enter the correct username and password")
+        print("\n")
+        self.main()
 
 obj  = Chatbot()
             
